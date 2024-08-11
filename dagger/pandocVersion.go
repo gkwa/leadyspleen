53fc9d4e06f408e10322167771a00696ed2dbebd
Taylor Monacelli <taylormonacelli@gmail.com>
2024-08-11 11:34:05 -0700

    Make pandoc version optional

--- dagger/pandocVersion.go
+++ dagger/pandocVersion.go
@@ -2,10 +2,8 @@ package main
 
 import "fmt"
 
-// PandocVersion is a custom type to handle version logic
 type PandocVersion string
 
-// NewPandocVersion creates a new PandocVersion with a default if empty
 func NewPandocVersion(version string) PandocVersion {
 	if version == "" {
 		return PandocVersion("latest")
@@ -13,12 +11,10 @@ func NewPandocVersion(version string) PandocVersion {
 	return PandocVersion(version)
 }
 
-// String returns the string representation of the version
 func (pv PandocVersion) String() string {
 	return string(pv)
 }
 
-// ImageName returns the full image name for this version
 func (pv PandocVersion) ImageName() string {
 	return fmt.Sprintf("pandoc/core:%s", pv)
 }
